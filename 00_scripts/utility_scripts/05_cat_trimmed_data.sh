543ba7c490629f3d11ff34e6e4e7c4765692f4a9
Maeva Leitwein <40304472+mleitwein@users.noreply.github.com>
2018-07-03 15:45:18 -0400

    Add files via upload

---
+++ 00_scripts/utility_scripts/05_cat_trimmed_data.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH -D ./ 
+#SBATCH --job-name="cat_file"
+#SBATCH -o 98_log_files/cat.out
+#SBATCH -c 1
+#SBATCH -p ibismini
+#SBATCH -A ibismini
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=maeva.leitwein.1@ulaval.ca
+#SBATCH --time=00-10:00
+#SBATCH --mem=5G
+
+
+cd $SLURM_SUBMIT_DIR
+
+#global variables
+DATAFOLDER="03_trimmed"
+DATAOUTPUT="03_trimmed_rename"
+SAMPLE="01_info_file/sample_name.txt"
+
+cat "$SAMPLE" | while read i  
+do
+
+  rm "$DATAOUTPUT"/"$i"_R1.fastq.gz "$DATAOUTPUT"/"$i"_R2.fastq.gz
+  
+  cat "$DATAFOLDER"/*."$i"_*R1.fastq.gz >> "$DATAOUTPUT"/"$i"_R1.fastq.gz
+  cat "$DATAFOLDER"/*."$i"_*R2.fastq.gz >> "$DATAOUTPUT"/"$i"_R2.fastq.gz
+
+done
