abbd8a6d295a0fc9db98bcaf1f4287aba485ffb5
Maeva Leitwein <40304472+mleitwein@users.noreply.github.com>
2018-12-04 14:19:16 -0500

    Update 06_methpipe.sh

--- 00_scripts/06_methpipe.sh
+++ 00_scripts/06_methpipe.sh
@@ -1,44 +1,56 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #SBATCH -D ./ 
-#SBATCH --job-name="methpipe.__BASE__"
-#SBATCH -o 98_log_files/log-methpipe.__BASE__.out
-#SBATCH -c 1
-#SBATCH -p ibismax
-#SBATCH -A ibismax
+#SBATCH --job-name="methpipe_x"
+#SBATCH -o 98_log_files/methpipe_x.out
+#SBATCH -c 2
+#SBATCH -p medium
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=type_your_mail@ulaval.ca
-#SBATCH --time=20-00:00
-#SBATCH --mem=20000
+#SBATCH --time=3-00:00
+#SBATCH --mem=20G
+
+
+module load methpipe/3.4.3
 
 cd $SLURM_SUBMIT_DIR
 
 #global variables
-GENOME="04_reference/genome_filtered.fa"
+GENOME="04_reference/genome_filtered.fna"
 DATA_FOLDER="05_results"
 DATAINPUT="03_trimmed"
-base=__BASE__
+#base=__BASE__
 
+SAMPLE="01_info_file/sample_name_x"
 
+cat "$SAMPLE" | while read i  
+do
 
-LC_ALL=C sort -k 1,1 -k 2,2n -k 3,3n -k 6,6 -o "$DATA_FOLDER"/"$base".mr.sorted_start "$DATAINPUT"/"$base".mr
+LC_ALL=C sort -k 1,1 -k 2,2n -k 3,3n -k 6,6 -o "$DATA_FOLDER"/"$i".mr.sorted_start "$DATA_FOLDER"/"$i".mr
 
-rm "$DATAINPUT"/"$base".mr
+#rm "$DATAINPUT"/"$base".mr
 
 # Remove putative PCR duplicates
-duplicate-remover -S "$DATA_FOLDER"/"$base".dremove_stat.txt -o "$DATA_FOLDER"/"$base".mr.dremove "$DATA_FOLDER"/"$base".mr.sorted_start
 
-rm "$DATA_FOLDER"/"$base".mr.sorted_start
+duplicate-remover -S "$DATA_FOLDER"/"$i".dremove_stat.txt -o "$DATA_FOLDER"/"$i".mr.dremove "$DATA_FOLDER"/"$i".mr.sorted_start
+
+rm "$DATA_FOLDER"/"$i".mr.sorted_start
+
+LC_ALL=C sort -k 1,1 -k 2,2n -k 3,3n -k 6,6 -o "$DATA_FOLDER"/"$i".mr.dremove.sort "$DATA_FOLDER"/"$i".mr.dremove
+
+rm "$DATA_FOLDER"/"$i".mr.dremove
 
 # Compute methylation level
-methcounts -cpg-only -c $GENOME -o "$DATA_FOLDER"/"$base".meth "$DATA_FOLDER"/"$base".mr.dremove
+methcounts -cpg-only -c $GENOME -o "$DATA_FOLDER"/"$i".meth "$DATA_FOLDER"/"$i".mr.dremove.sort
 
 # Compute symmetric CpGs contexts
-symmetric-cpgs -o "$DATA_FOLDER"/"$base"_CpG.meth "$DATA_FOLDER"/"$base".meth
+symmetric-cpgs -o "$DATA_FOLDER"/"$i"_CpG.meth "$DATA_FOLDER"/"$i".meth
 
 # Compute methylation stats
-levels -o "$DATA_FOLDER"/"$base".levels "$DATA_FOLDER"/"$base".meth
+levels -o "$DATA_FOLDER"/"$i".levels "$DATA_FOLDER"/"$i".meth
 
 # Compute conversion rate
-bsrate -c $GENOME -o "$DATA_FOLDER"/"$base".bsrate "$DATA_FOLDER"/"$base".mr.dremove
+bsrate -c $GENOME -o "$DATA_FOLDER"/"$i".bsrate "$DATA_FOLDER"/"$i".mr.dremove.sort
 
-rm "$DATA_FOLDER"/"$base".mr.dremove.sort
+rm "$DATA_FOLDER"/"$i".mr.dremove.sort
+
+done
