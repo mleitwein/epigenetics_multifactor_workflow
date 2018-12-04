e746f0e0c6414de215332de5ade0c7a7d01e64ed
Maeva Leitwein <40304472+mleitwein@users.noreply.github.com>
2018-12-04 14:22:47 -0500

    Update 07_prepare_dss.sh

--- 00_scripts/07_prepare_dss.sh
+++ 00_scripts/07_prepare_dss.sh
@@ -1,28 +1,34 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #SBATCH -D ./ 
-#SBATCH --job-name="prepdss.__BASE__"
-#SBATCH -o 98_log_files/log-prepdss.__BASE__.out
+#SBATCH --job-name="prepare_file_Dss"
+#SBATCH -o 98_log_files/prepare_dss.out
 #SBATCH -c 1
-#SBATCH -p ibismini
-#SBATCH -A ibismini
+#SBATCH -p medium
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=type_your_mail@ulaval.ca
-#SBATCH --time=20-00:00
-#SBATCH --mem=2000
+#SBATCH --time=3-00:00
+#SBATCH --mem=20G
 
 cd $SLURM_SUBMIT_DIR
 
 # Global variables
 DATA_FOLDER="05_results"
-base=__BASE__
+#base=__BASE__
+
+SAMPLE="01_info_file/sample_name.txt"
+
+cat "$SAMPLE" | while read i  
+do
 
 # prepare file for dss
-awk '{print $1,$2,$6}' "$DATA_FOLDER"/"$base"_CpG.meth >"$DATA_FOLDER"/"$base".dss.1.temp
-awk '{printf "%.0f\n",$5*$6}' "$DATA_FOLDER"/"$base"_CpG.meth >"$DATA_FOLDER"/"$base".dss.2.temp
-paste "$DATA_FOLDER"/"$base".dss.1.temp "$DATA_FOLDER"/"$base".dss.2.temp >"$DATA_FOLDER"/"$base".temp.dss
-cat 01_info_files/header.dss "$DATA_FOLDER"/"$base".temp.dss|sed 's/\t/ /g' >"$DATA_FOLDER"/"$base".dss
+awk '{print $1,$2,$6}' "$DATA_FOLDER"/"$i"_CpG.meth >"$DATA_FOLDER"/"$i".dss.1.temp
+awk '{printf "%.0f\n",$5*$6}' "$DATA_FOLDER"/"$i"_CpG.meth >"$DATA_FOLDER"/"$i".dss.2.temp
+paste "$DATA_FOLDER"/"$i".dss.1.temp "$DATA_FOLDER"/"$base".dss.2.temp >"$DATA_FOLDER"/"$i".temp.dss
+cat 01_info_files/header.dss "$DATA_FOLDER"/"$i".temp.dss|sed 's/\t/ /g' >"$DATA_FOLDER"/"$i".dss
 
 # clean up
-rm "$DATA_FOLDER"/"$base".*.temp
-rm "$DATA_FOLDER"/"$base".*temp.dss
+rm "$DATA_FOLDER"/"$i".*.temp
+rm "$DATA_FOLDER"/"$i".*temp.dss
+
+done
 
